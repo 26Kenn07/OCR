fb8f28707b39f5ab8d02c41ae1747f41b9df0912
Kirtan Matalia <kirtan@atliq.com>
2024-09-29 20:47:45 +0530

    Updated Keyword Search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pytesseract
 from PIL import Image
 import streamlit as st
+import re
 
 st.title("OCR and Keyword Search (Hindi & English)")
 
@@ -11,18 +12,24 @@ if uploaded_image is not None:
     
     extracted_text = pytesseract.image_to_string(img, lang='eng+hin')
 
-
     st.subheader("Extracted Text:")
-    st.text_area("Text Output", extracted_text)
+    st.text_area("Text", extracted_text)
 
     search_term = st.text_input("Enter a keyword to search within the extracted text:")
 
     if search_term:
         st.subheader("Search Results:")
+        
         if search_term.lower() in extracted_text.lower():
             st.write(f"Keyword '{search_term}' found in the extracted text.")
+
+            def highlight_keyword(text, keyword):
+                highlighted_text = re.sub(f"({re.escape(keyword)})", r'<span style="background-color: yellow; color: black;">\1</span>', text, flags=re.IGNORECASE)
+                return highlighted_text
+            
+            highlighted_text = highlight_keyword(extracted_text, search_term)
+
+            st.markdown(f"<div style='white-space: pre-wrap;'>{highlighted_text}</div>", unsafe_allow_html=True)
+
         else:
             st.write(f"Keyword '{search_term}' not found.")
-
-
-
